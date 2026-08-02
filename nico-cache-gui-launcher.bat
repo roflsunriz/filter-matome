d5ba3b52102866e5adbc8d71fb9aa3cf8756c1b5
roflsunriz <roflsunriz@yahoo.co.jp>
2026-08-02 21:24:53 +0900

    refactor(scripts): NicoCache関連のバッチファイルとスクリプトを削除

--- nico-cache-gui-launcher.bat
+++
@@ -1,2 +0,0 @@
-@echo off > nul
-start "nico-cache-gui-launcher - 2025/09/24" javaw -jar NicoCache_nl.jar
--- nico-cache-nl-starter.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-start /MIN "nico-cache-nl-starter - 2025/09/30" java -jar NicoCache_nl.jar
